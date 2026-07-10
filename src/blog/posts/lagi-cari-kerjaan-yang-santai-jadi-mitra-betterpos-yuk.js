11b6f0d55723c53e8825ca79f690a70021ec0f20
hidayatullah1307-tech <hidayatullah1307@gmail.com>
2026-07-10 23:51:07 +0700

    blog: tambah artikel "Lagi cari kerjaan yang santai? Jadi mitra BetterPOS yuk!"

---
+++ src/blog/posts/lagi-cari-kerjaan-yang-santai-jadi-mitra-betterpos-yuk.js
@@ -0,0 +1,18 @@
+export default {
+  "slug": "lagi-cari-kerjaan-yang-santai-jadi-mitra-betterpos-yuk",
+  "title": "Lagi cari kerjaan yang santai? Jadi mitra BetterPOS yuk!",
+  "excerpt": "Disini bakal dibahas gimana caranya jadi mitra BetterPOS buat dapetin 3,5 juta rupiah!",
+  "tags": [
+    "mitra",
+    "freelance",
+    "kerja",
+    "sales",
+    "aplikasi",
+    "apliksi kasir",
+    "umkm",
+    "bisnis",
+    "aplikasi"
+  ],
+  "content": "Buat kamu yang mau dapetin uang mulai dari 300 ribu sampai 3,5 juta langsung aja klik tombol kemitraan dan hubugi admin melalui WhatsApp. Caranya gimana?\n\n1. Langsung aja hubungi admin lewat website bakal langsung diarahkan ke WhatsApp admin BetterPOS.\n2. Bakal ada perbincangan singkat, soal identitas dan lokasi kamu serta pertanyaan seputar sales dan pengetahuan umum mengenai BetterPos dan aplikasi lainnya.\n3. Setelah itu tim BetterPos akan menghubungi kamu kembali untuk konfirmasi dan persiapan product knowledge.\n4. Setelah itu kamu akan diberi pengetahuan seputar produk, cara menggunakan dan bagaimana cara client mengaktifkan BetterPos di Store-nya.\n5. Store akan melakukan pembayaran ke BetterPOS dan tim BetterPOS akan mengirimkan uangnya ke kamu, ga lama setelah transaksi selesai.\n\nKebutuhan yang perlu kamu perisapkan adalah:\n1. Rekening bank atau wallet digitalmu.\n2. Laptop untuk download aplikasi BetterPOS  dan untuk presentasi ke pemilik usaha.\n\nPertanyaan umum:\n1. Bagaimana kalau kamu tidak punya laptop.\nJika pemilik usaha memiliki laptop atau PC, kamu bisa menggunakannya untuk presentasi dan menginstall demo BetterPOS. Kamu cuma butuh hp mu untuk download aplikasi melalui WhatsApp untuk dicopy ke komputer Store.\n2. Bagaimana dengan pembayarannya?\nKomisi akan segera dibayarkan setelah client melakukan pembayaran dan dikonfirmasi oleh Admin. Akan ada penyesuaian apabila menggunakan Bank berbeda atau dompet digital yang memilki biaya administrasi.\n3. Apakah ada biaya pendafataran untuk jadi mitra?\nTidak ada. Tidak keluar uang sama sekali.\n4. Apakah ada target penjualan?\nTidak ada target penjualan, hanya ada bonus.\n5. Apakah ada batasan waktu dalam bekerja menjadi mitra BetterPOS?\nTidak ada batasan waktu, apabila kode sales mu hilang, dipersilahkan untuk meminta kembali kepada Admin.\n\nJadi tunggu apa lagi? Yuk langsung jadi mitra BetterPOS~",
+  "date": "2026-07-10"
+}
